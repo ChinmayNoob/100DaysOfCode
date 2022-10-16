77f00d14f6e71665a09050f638fd7c6a2ca06d45
ChinmayNoob <113923797+ChinmayNoob@users.noreply.github.com>
2022-10-16 18:15:20 +0530

    init

---
+++ Day9.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+      int a;
+      int b;
+      cin>>a>>b;
+      if(b/a>=0.5){
+         cout<<"YES"<<endl;
+      }
+      else {
+         cout<<"NO"<<endl;
+      }
+    
+
+    }
+    }
